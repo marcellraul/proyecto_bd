f360ca0fb840fa93f2cb4ad5b0a63fd918e6d18d
sorelis8 <marcellraul20@gmail.com>
2019-02-14 18:54:04 -0400

    Primer commit

---
+++ universidad/apps/GestionAcademica/admin.py
@@ -0,0 +1,10 @@
+
+from django.contrib import admin
+from universidad.apps.GestionAcademica.models import *
+
+
+# Register your models here.
+
+admin.site.register(Alumno)
+admin.site.register(Curso)
+admin.site.register(Matricula)
